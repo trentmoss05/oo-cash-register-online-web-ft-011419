4a39a4047e08e9c55137dab49c0b2987f1598c7a
Trent Moss <trentmoss05@gmail.com>
2019-01-24 21:41:22 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
+class CashRegister
 
+  attr_accessor :total
+  def initialize(employee_discount = 0)
+    if employee_discount == 20
+      apply_discount
+    else
+      @total = 0
+    end
+
+  end
+
+  def add_item(item, price, quantity = 1)
+    @total = @total + (price*quantity)
+  end
+
+  def apply_discount
+
+
+end
