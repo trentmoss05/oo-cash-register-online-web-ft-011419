89ceefa2629074e0f5808a17686af782d722ccab
Trent Moss <trentmoss05@gmail.com>
2019-01-24 21:45:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   def add_item(item, price, quantity = 1)
     self.total += price * quantity
     quantity.times do
-      items << title
+      items << item
     end
     self.last_transaction = price * quantity
   end
